ae49a5dce2523fc01e256a69d15154986a4cc15c
Maister <blabusmaciej@gmail.com>
2025-10-01 18:47:24 +0200

    site

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,5 +1,6 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
