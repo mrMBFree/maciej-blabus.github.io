d1a055a9216e19ab9eeebd85dc6f8e3164611b0d
Maister <blabusmaciej@gmail.com>
2025-10-01 18:34:20 +0200

    site

--- nextjs-tailwind-resume-page-main/src/components/index.ts
+++
@@ -1,7 +0,0 @@
-"use client";
-
-export * from "./navbar";
-export * from "./footer";
-export * from "./layout";
-export * from "./info-card";
-export * from "./fixed-plugin";
--- src/app/index.tsx
+++
@@ -1,8 +0,0 @@
-
-export * from "./hero";
-export * from "./page";
-export * from "./layout";
-export * from "./testimonial";
-export * from "./information-section";
-
-
